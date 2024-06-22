75640a96db5216d9725d29476d4a42a35b92292e
citogok <47094667+kenthusblog@users.noreply.github.com>
2024-06-22 13:11:24 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var _0x85bb = ['sideNav', 'ready', '.button-collapse-left', 'hide', 'left', 'modal', '.button-collapse', '.modal'];
+(function (_0x40f8ff, _0x85bb88) {
+    var _0x3dddba = function (_0x28bbc5) {
+        while (--_0x28bbc5) {
+            _0x40f8ff['push'](_0x40f8ff['shift']());
+        }
+    };
+    _0x3dddba(++_0x85bb88);
+}(_0x85bb, 0x175));
+var _0x3ddd = function (_0x40f8ff, _0x85bb88) {
+    _0x40f8ff = _0x40f8ff - 0x0;
+    var _0x3dddba = _0x85bb[_0x40f8ff];
+    return _0x3dddba;
+};
+$(document)[_0x3ddd('0x4')](function () {
+    $(_0x3ddd('0x5'))[_0x3ddd('0x3')]({
+        'menuWidth': 0x12c,
+        'edge': _0x3ddd('0x7'),
+        'closeOnClick': ![],
+        'draggable': !![]
+    });
+    $(_0x3ddd('0x1'))['sideNav'](_0x3ddd('0x6'));
+});
+$(document)[_0x3ddd('0x4')](function () {
+    $(_0x3ddd('0x2'))[_0x3ddd('0x0')]();
+});
